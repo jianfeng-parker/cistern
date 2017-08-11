529aa180fa8cbe4674256baf4dc8fcff16e6d457
parker.wu <wujianfeng@wujianfeng-MacBookPro.local>
2017-08-12 01:10:17 +0800

    fix语法错误

--- example/example.go
+++ example/example.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"cistern"
+	"github.com/jianfeng-parker/cistern"
 	"fmt"
 	"time"
 )
@@ -12,17 +12,17 @@ func main() {
 	c := cistern.NewCache(defaultExpired, cleanInterval)
 	k1 := "k1"
 	v1 := "v1"
-	expiration,_:= time.ParseDuration("5s")
+	expiration, _ := time.ParseDuration("5s")
 	c.Set(k1, v1, expiration)
 	if v, found := c.Get(k1); found {
-		fmt.Println("Found k1:" + v)
+		fmt.Println("Found k1:", v)
 	} else {
 		fmt.Println("Not found k1")
 	}
-     sleep,_ :=time.ParseDuration("10s")
+	sleep, _ := time.ParseDuration("10s")
 	time.Sleep(sleep)
 	if v, found := c.Get(k1); found {
-		fmt.Println("Found k1:" + v)
+		fmt.Println("Found k1:", v)
 	} else {
 		fmt.Println("Not found k1")
 	}
