e4493c3390823c0cf81e58f112c0d845043295a0
parker.wu <wujianfeng@wujianfeng-MacBookPro.local>
2017-08-27 18:57:30 +0800

    实现一致性hash ring的基本功能

--- hash/ring.go
+++ hash/ring.go
@@ -4,11 +4,16 @@ import (
 	"math"
 	"crypto/sha1"
 	"strconv"
+	"sort"
+)
+
+const (
+	factor_hash int = 4
 )
 
 type node struct {
-	hash     uint32
-	identity string
+	hash        uint32
+	destination string
 }
 
 type Ring struct {
@@ -22,25 +27,32 @@ func NewRing(slots int) (r *Ring) {
 		slots = 160 // 默认160个虚拟节点
 	}
 	r = &Ring{
-		slots: slots,
+		slots:   slots,
+		weights: make(map[string]int),
 	}
 	return
 }
 
-func (r *Ring) Add(identity string, weight int) {
-
+func (r *Ring) Add(destination string, weight int) {
+	r.weights[destination] = weight
+	r.build()
 }
 
-func (r *Ring) AddMulti() {
-
+func (r *Ring) AddMulti(weights map[string]int) {
+	for destination, weight := range weights {
+		r.weights[destination] = weight
+	}
+	r.build()
 }
 
-func (r *Ring) Remove(identity string) {
-
+func (r *Ring) Remove(destination string) {
+	delete(r.weights, destination)
+	r.build()
 }
 
-func (r *Ring) Update(identity string, weight int) {
-
+func (r *Ring) Update(destination string, weight int) {
+	r.weights[destination] = weight
+	r.build()
 }
 
 func (r *Ring) Len() int {
@@ -55,7 +67,17 @@ func (r *Ring) GetNode(key string) string {
 	if len(r.nodes) == 0 {
 		return ""
 	}
-	
+	sha := sha1.New()
+	sha.Write([]byte(key))
+	hBytes := sha.Sum(nil)
+	hValue := getHash(hBytes[2:2+factor_hash])
+	i := sort.Search(len(r.nodes), func(i int) bool {
+		return r.nodes[i].hash >= hValue
+	})
+	if i == len(r.nodes) {
+		return r.nodes[0].destination
+	}
+	return r.nodes[i].destination
 }
 
 func (r *Ring) build() {
@@ -65,14 +87,14 @@ func (r *Ring) build() {
 	}
 	totalSlots := r.slots * len(r.weights)
 	sha := sha1.New()
-	for identity, weight := range r.weights {
+	for destination, weight := range r.weights {
 		// 计算Key对应的虚拟节点数
 		slots := int(math.Floor(float64(weight) / float64(totalWeight) * float64(totalSlots)))
 		for i := 1; i <= slots; i++ {
-			sha.Write([]byte(identity + ":" + strconv.Itoa(i)))
+			sha.Write([]byte(destination + ":" + strconv.Itoa(i)))
 			n := node{
-				hash:     getHash(sha.Sum(nil)[2:6]),
-				identity: identity,
+				hash:        getHash(sha.Sum(nil)[2:2+factor_hash]),
+				destination: destination,
 			}
 			r.nodes = append(r.nodes, n)
 			sha.Reset()
@@ -82,5 +104,8 @@ func (r *Ring) build() {
 }
 
 func getHash(bs []byte) uint32 {
-
+	if len(bs) < factor_hash {
+		return 0
+	}
+	return (uint32(bs[3]) << 24) | (uint32(bs[2]) << 16) | (uint32(bs[1]) << 8) | (uint32(bs[0]))
 }
